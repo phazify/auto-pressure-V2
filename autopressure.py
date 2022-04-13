869925c9abba2428bbcbf933f74160178474116b
phazify <101293535+phazify@users.noreply.github.com>
2022-04-14 01:45:20 +0200

    Create autopressure.py

---
+++ autopressure.py
@@ -0,0 +1,112 @@
+import getpass
+import colorama
+from colorama import Fore
+import discord
+from discord.ext import commands
+import os
+import getpass
+client = commands.Bot(command_prefix=commands.when_mentioned_or("$"))
+
+os.system(f'cls & mode 85,20 & title PHAZIFY AUTOPRESSER')
+user = input("Name: ")
+print(f'''{Fore.CYAN}
+ _    __   __    __   ______   ________  ______  ________  __      __ 
+|       \ |  \  |  \ /      \ |        \|      \|        \|  \    /  |
+| $$$$$$$\| $$  | $$|  $$$$$$\ \$$$$$$$$ \$$$$$$| $$$$$$$$ \$$\  /  $$
+| $$__/ $$| $$__| $$| $$__| $$    /  $$   | $$  | $$__      \$$\/  $$ 
+| $$    $$| $$    $$| $$    $$   /  $$    | $$  | $$  \      \$$  $$  
+| $$$$$$$ | $$$$$$$$| $$$$$$$$  /  $$     | $$  | $$$$$       \$$$$   
+| $$      | $$  | $$| $$  | $$ /  $$___  _| $$_ | $$          | $$    
+| $$      | $$  | $$| $$  | $$|  $$    \|   $$ \| $$          | $$    
+ \$$       \$$   \$$ \$$   \$$ \$$$$$$$$ \$$$$$$ \$$           \$$    
+             ___________LOGGED IN AS: {user}___________                                             
+                     P1 X PHAZIFY MADE THIS
+                                                                                    
+        ''' + Fore.CYAN)
+
+token = input("Enter token: ")
+sex = input("Keyword: ")
+print("READY TO RAPE BLACKS")
+###### CODEZZZZZ#######
+@client.event
+async def on_message(message):
+    channel = message.channel
+    print("BITCH GOT RAPED:",message.content)
+    if message.content.endswith(sex):
+
+       await message.channel.send('LOOLOOL SO THIS UNKNOWN NIGGA SPEAKING TO ME? https://cdn.discordapp.com/attachments/963221490034167912/963467038620254238/unknown.png')
+       await message.channel.send('LMAAOOO OBEY YOUR ALMIGHTY FATHER START DIGGING YOUR GRAVE 💀💀💀')
+       await message.channel.send('LOOL U AINT RDY FOR THIS REAL SHIT GO BACK TO PLAYING OSU')
+       await message.channel.send('LMAAOOO I SLIPPED UR MOM THROAT OFF FAGGOT')
+       await message.channel.send('LOOLOOOL I ROUGHLY HARDCORE FUCKED UR MOM')
+       await message.channel.send('UR MOM PAINTED HER PUBES PINK U LGBTQ FUCK')
+       await message.channel.send('OLOLOL')
+       await message.channel.send('LOLOL')
+       await message.channel.send('OLOL')
+       await message.channel.send('OOLOL')
+       await message.channel.send('LOLOL')
+       await message.channel.send('YOUR BIRTH WAS A MISTAKE THE CONDOM BROKE')
+       await message.channel.send('LOOLOOL ')
+       await message.channel.send('I E FUCKED UR MOM SO ROUGHLY SHE GOT PREGNANT THROUGH BLUETOOTH CONNECTION')
+       await message.channel.send('UR MOM GOT POISONED')
+       await message.channel.send('UR MOMS A NAKED MOLE RAT https://cdn.discordapp.com/attachments/963221490034167912/963531294497714226/unknown.png')
+       await message.channel.send('TRAGIC DEATH')
+       await message.channel.send('NO ONE WILL VISIT YOUR GRAVEYARD LOOL')
+       await message.channel.send('LOOOL BREAKDANCE ON ITS RAINING TACOS U WEIRD BUM')
+       await message.channel.send('GO HOP ON ADOLF HITLERS DICK LOOLOOOLL')
+       await message.channel.send('U CORNY FUCK')
+       await message.channel.send('PUSSY ASS NIGGER')
+       await message.channel.send('LOOLOOOL')
+       await message.channel.send('LARRY THE LOBSTER SLIPPED OF YO DICK https://cdn.discordapp.com/attachments/963221490034167912/963532165965029396/unknown.png')
+       await message.channel.send('🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀🦀')
+       await message.channel.send('LOOOLOOL UR MOM SQUIRTS A MIX OF CURRY KETCHUP MAYONAISE https://cdn.discordapp.com/attachments/963221490034167912/963532378255540225/unknown.png')
+       await message.channel.send('LOOOLOOOOOLOLOLOLOLOLOLOL')
+       await message.channel.send('SON IS SLEEP WALKING')
+       await message.channel.send('WEAK ASS FOOTBALL NECROPHILIA')
+       await message.channel.send('SON IS OVERWHELMED')
+       await message.channel.send('UR MOMS BIG ASS FOREHEAD I RAN THE WHOLE PLAY OFFS ON IT + A TOUCH DOWN')
+       await message.channel.send('U HIJACKED PIRATE BOATS U SOMALIAN FUCK')
+       await message.channel.send('BIG CHUNGUS ASS LOOKING ASS')
+       await message.channel.send('PEDU BUSTER LOOKING SHIT https://cdn.discordapp.com/attachments/963221490034167912/963534091381252157/unknown.png')
+       await message.channel.send('PRAY THAT YOU WILL GET THROUGH THE NIGHT YOU FUCKING BAT')
+       await message.channel.send('GORILLA LOOKING ASS BLACK ASS NIGGER BLACK THAN THE COLOUR BLACK')
+       await message.channel.send('SPEAK TO ME WHEN YOU LOSE SOME SKIN TONE LOLLOOL 🤓🤓🤓🤓')
+       await message.channel.send('LOLOLOOLOOL MY SON LOOL')
+       await message.channel.send('WORSHIP ME ')
+       await message.channel.send('OBEY ME')
+       await message.channel.send('OR ELSE THE CONSEQUENCES WILL BE WORST')
+       await message.channel.send('CHANGE UR MOMS MONTHLY NAPKIN')
+       await message.channel.send('ROBLOX RO GANGSTER')
+       await message.channel.send('UR MOMS THICK 10 MM BULLETPROOF ASS IS UNSTOPPABLE')
+       await message.channel.send('KEEP RIDING ON THAT DICK YOU CANT BE SAVED')
+       await message.channel.send('UR TRAPPED IN THE GC')
+       await message.channel.send('DONT YAWN OR BLINK ')
+       await message.channel.send('SHUT YOUR 2013 WINDOWS 7 I3 LOOKING ASS UP U FINNA PACK ON THAT POTATO PC?')
+       await message.channel.send('U WEAK TWAT')
+       await message.channel.send('GOBLIN BARBARIAN LOOKING SHIT')
+       await message.channel.send('YOUR ANCESTORS GOT BOMBED BY THE SOVJET UNIE')
+       await message.channel.send('UR ASS THOUGHT U WAS THE AVATAR TRYNA MASTER ALL THE 4 ELEMENTS https://cdn.discordapp.com/attachments/963221490034167912/963536933823320085/unknown.png')
+       await message.channel.send('UR MOM ATE HIGHKICK IN THE ASS')
+       await message.channel.send('RATIO L BOZO')
+       await message.channel.send('UR MOMS A CHEAP PROSITUDE')
+       await message.channel.send('LOOLOOL MONKEY ASS NIGGA ORANG OETANS LOOKING ASS')
+       await message.channel.send('PHAZIFY IS UNSTOPPABLE')
+       await message.channel.send('LL')
+       await message.channel.send('OO')
+       await message.channel.send('OOL')
+       await message.channel.send('OOL')
+       await message.channel.send('LLOO')
+       await message.channel.send('LOL')
+       await message.channel.send('OLOO')
+       await message.channel.send('LOLO')
+       await message.channel.send('LOLOL')
+       await message.channel.send('OLOL')
+       await message.channel.send('OOL')
+       await message.channel.send('OOL')
+       await message.channel.send('FOCUS')
+       await message.channel.send('SEED')
+       await message.channel.send('GOON FAGGOT')
+       await message.channel.send('GHOST CHAT U AINT FAST ENOUGH U SPED')
+       await message.channel.send('UR MOM GETTING RUSHED TO THE HOSPITAL ICU ILL SMACK THAT BITCH IN HER GRAVE')
+        
+client.run(token, bot=False)
